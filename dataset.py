adeebb76ef17e6390ecf3f91acb0003fd9222c33
Ilango R <ilangokul@gmail.com>
2019-05-04 16:23:50 +0530

    Unified script for dataset download, extract, conversion

--- dataset.py
+++ dataset.py
@@ -1,24 +1,57 @@
 #!/usr/bin/env python
 
+from tqdm import tqdm
+import os
+import math
+import requests
+import tarfile
 import h5py
 import numpy as np
 import pandas as pd
 import cv2 as cv
-from tqdm import tqdm
 
 
 splits = ["train", "test"]
+baseurl = "http://ufldl.stanford.edu/housenumbers/"
+chunk_size = 1048576  # 1 MB
 
-for path in splits:
-    digit = h5py.File(path+"/digitStruct.mat", "r")
+for file in splits:
+    fname = file+".tar.gz"
+    if os.path.exists(fname):
+        print(fname, "already exists")
+        continue
+    fd = open(fname, "wb")
+    resp = requests.get(baseurl+fname, stream=True)
+    if resp.status_code != 200:
+        raise RuntimeError("Error downloading dataset: " + resp.status_code)
+    l = int(resp.headers.get("Content-Length"))
+    l = math.ceil(l/chunk_size)
+    for chunk in tqdm(resp.iter_content(chunk_size=chunk_size), desc=fname, total=l):
+        fd.write(chunk)
+    fd.close()
+
+for file in splits:
+    if os.path.exists(file):
+        print(file, "already exists. No need of extraction")
+        continue
+    fname = file+".tar.gz"
+    fd = tarfile.open(fname, "r")
+    for memb in tqdm(fd.getmembers(), desc=fname + " extraction"):
+        fd.extract(memb)
+    fd.close()
+
+for file in splits:
+    if os.path.exists(file+".csv"):
+        print(file+".csv", "already exists. No need of conversion.")
+        continue
+    digit = h5py.File(file+"/digitStruct.mat", "r")
     tot = len(digit.get("digitStruct/name"))
 
     df = pd.DataFrame(columns=["name", "label", "top",
                                "left", "height", "width", "imgheight", "imgwidth"])
 
     i = 0
-    print(path, ":")
-    for k, (n, b) in tqdm(enumerate(zip(digit.get("digitStruct/name"), digit.get("digitStruct/bbox"))), total=tot):
+    for k, (n, b) in tqdm(enumerate(zip(digit.get("digitStruct/name"), digit.get("digitStruct/bbox"))), desc=file+" conversion", total=tot):
         nm = np.array(digit[n[0]]).reshape(-1)
         name = "".join(map(chr, nm))
         imgheight, imgwidth = cv.imread("train/"+name).shape[:2]
@@ -50,4 +83,4 @@ for path in splits:
 
     df["x"] = df.left+df.width/2
     df["y"] = df.top+df.height/2
-    df.to_csv(path+".csv", index=False)
+    df.to_csv(file+".csv", index=False)
